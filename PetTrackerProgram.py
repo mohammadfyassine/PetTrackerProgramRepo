0b56fdc9862bd42cbe50e0583059a1f241cd1136
Mohammad Yassine <mohammadfyassine@gmail.com>
2025-07-01 16:35:23 +0300

    Defining Class and Attritubes

---
+++ PetTrackerProgram.py
@@ -0,0 +1,9 @@
+class Pet:
+    def __init__(self, name, species, age, adopted=False):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.adopted = adopted
+    
+
+
